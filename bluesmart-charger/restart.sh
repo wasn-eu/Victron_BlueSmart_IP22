442f43c1811a60ae65fca2f43d3e16411323780f
WASN <info@wasn.eu>
2023-09-10 22:40:40 +0200

    Update restart.sh

--- bluesmart-charger/restart.sh
+++ bluesmart-charger/restart.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
-kill  $(pgrep -f "supervise $SERVICE_NAME") 
+kill $(pgrep -f "python $SCRIPT_DIR/$SERVICE_NAME.py")
